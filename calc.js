21f364ccdc07e330d95969beb319e604c7f32b10
ephie oyedoh <ephieo@rocketmail.com>
2020-06-27 20:37:45 +0100

    fetching the data attributes and creating a class to hold my calc functions

--- calc.js
+++ calc.js
@@ -1,1 +1,17 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentpreviousOperandTextElement 
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const deleteButton = document.querySelector('[data-delete]');
+const equalsButton = document.querySelector('[data-equals]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+// console.log(currentOperandTextElement);
+
 
